b47e79f079a6c5566951372bee51fbed6b029d86
tracer-ics2017 <tracer@njuics.org>
2021-03-13 21:38:53 +0800

    >  compile 161930315 zhaozeyuan Linux debian 4.19.0-14-686 #1 SMP Debian 4.19.171-2 (2021-01-30) i686 GNU/Linux  21:38:53 up  7:15,  2 users,  load average: 0.01, 0.01, 0.00 f850ba5f40f8ae87ea7b2af4490be43884caea7

--- nemu/src/monitor/debug/ui.c
+++ nemu/src/monitor/debug/ui.c
@@ -169,13 +169,13 @@ static int cmd_x(char *args) {
 	}
 	arg+=2;
 	sscanf(arg,"%x",&vaddr);
-	printf("Address\tDword block\tByte sequence\n");
+	printf("Address      Dword block      Byte sequence\n");
 	for(int i = 0;i < num;++i) {
 		uint32_t addr = negative?vaddr-i*4:vaddr+i*4;
 		int value = vaddr_read(addr,4);
 		printf("0x%08x\t0x%08x\t",addr,value);
 		for(int j = 0;j < 4;j++) {
-			printf("%2x ",value%256);
+			printf("%02x ",value%256);
 			value/=256;
 		}
 		printf("\n");
