bbe3a09ecb21b128aa8048903cb3bbf9df820dc7
tracer-ics2017 <tracer@njuics.org>
2021-03-13 16:10:25 +0800

    >  compile 161930315 zhaozeyuan Linux debian 4.19.0-14-686 #1 SMP Debian 4.19.171-2 (2021-01-30) i686 GNU/Linux  16:10:25 up  1:46,  2 users,  load average: 0.03, 0.02, 0.00 3054901815c69f70e8fcfa8937c656f4d43ff743

--- nemu/src/monitor/debug/ui.c
+++ nemu/src/monitor/debug/ui.c
@@ -116,16 +116,16 @@ static int cmd_info(char *args){
 	}
 	else if(*arg == 'r') {
 		for(int i = 0;i < 8;++i) {
-			printf("%s:\t0x%-8x\t%d\n",regsl[i],cpu.gpr[i]._32,cpu.gpr[i]._32);
+			printf("%s:\t0x%08x\t%d\n",regsl[i],cpu.gpr[i]._32,cpu.gpr[i]._32);
 		}
 		for(int i = 0;i < 8;++i) {
-			printf("%s:\t0x%-8x\t%d\n",regsw[i],cpu.gpr[i]._16,cpu.gpr[i]._16);
+			printf("%s:\t0x%08x\t%d\n",regsw[i],cpu.gpr[i]._16,cpu.gpr[i]._16);
 		}
 		for(int i = 0;i < 4;++i) {
-			printf("%s:\t0x%-8x\t%d\n",regsb[i],cpu.gpr[i]._8[0],cpu.gpr[i]._8[0]);	
+			printf("%s:\t0x%08x\t%d\n",regsb[i],cpu.gpr[i]._8[0],cpu.gpr[i]._8[0]);	
 		}
 		for(int i = 0;i < 4;++i) {
-			printf("%s:\t0x%-8x\t%d\n",regsb[i+4],cpu.gpr[i]._8[1],cpu.gpr[i]._8[1]);
+			printf("%s:\t0x%08x\t%d\n",regsb[i+4],cpu.gpr[i]._8[1],cpu.gpr[i]._8[1]);
 		}
 	}
 	else if(*arg == 'w') {
