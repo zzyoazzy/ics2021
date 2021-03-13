e29715d91823bb1615750e1ba4f935d09b37b4d1
tracer-ics2017 <tracer@njuics.org>
2021-03-13 15:49:18 +0800

    >  compile 161930315 zhaozeyuan Linux debian 4.19.0-14-686 #1 SMP Debian 4.19.171-2 (2021-01-30) i686 GNU/Linux  15:49:18 up  1:25,  2 users,  load average: 0.00, 0.00, 0.00 1e359c564ac00eef8f4714726a848468caec9360

--- nemu/src/monitor/debug/ui.c
+++ nemu/src/monitor/debug/ui.c
@@ -118,6 +118,15 @@ static int cmd_info(char *args){
 		for(int i = 0;i < 8;++i) {
 			printf("%s:\t%8x\t%d\n",regsl[i],cpu.gpr[i]._32,cpu.gpr[i]._32);
 		}
+		for(int i = 0;i < 8;++i) {
+			printf("%s:\t%8x\t%d\n",regsw[i],cpu.gpr[i]._16,cpu.gpr[i]._16);
+		}
+		for(int i = 0;i < 4;++i) {
+			printf("%s:\t%8x\t%d\n",regsb[i],cpu.gpr[i]._8[0],cpu.gpr[i]._8[0]);	
+		}
+		for(int i = 0;i < 4;++i) {
+			printf("%s:\t%8x\t%d\n",regsb[i+4],cpu.gpr[i]._8[1],cpu.gpr[i]._8[1]);
+		}
 	}
 	else if(*arg == 'w') {
 
