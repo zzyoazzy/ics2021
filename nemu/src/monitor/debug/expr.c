d8ebd24cd3a6376570dedbd6262d2a78ad16da56
tracer-ics2017 <tracer@njuics.org>
2021-03-23 19:42:17 +0800

    >  compile 161930315 zhaozeyuan Linux debian 4.19.0-14-686 #1 SMP Debian 4.19.171-2 (2021-01-30) i686 GNU/Linux  19:42:17 up 33 min,  2 users,  load average: 0.00, 0.00, 0.00 667408b9cd7b859e613552ff939d4ffdc1889793

--- nemu/src/monitor/debug/expr.c
+++ nemu/src/monitor/debug/expr.c
@@ -44,7 +44,7 @@ static struct rule {
   {"==", TK_EQ},        // equal
   {"!=", TK_UEQ},		// unequal
   {"&&", TK_AND},		// and
-  {"||", TK_OR},		// or
+  {"\\|\\|", TK_OR},		// or
   {"!",  TK_NOT},		// not
 };
 
