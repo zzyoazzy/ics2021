1d0923202d312f01092ce60bcb316c3204d3e6bf
tracer-ics2017 <tracer@njuics.org>
2021-03-30 21:18:11 +0800

    >  compile 161930315 zhaozeyuan Linux debian 4.19.0-14-686 #1 SMP Debian 4.19.171-2 (2021-01-30) i686 GNU/Linux  21:18:11 up  2:06,  2 users,  load average: 0.06, 0.02, 0.00 1bd09373439d4be7a14b4ddaccd572864aacc10

--- nemu/src/monitor/debug/ui.c
+++ nemu/src/monitor/debug/ui.c
@@ -207,9 +207,9 @@ static int cmd_w(char *args) {
 		return 0;
 	}
 	ans++;
-//	WP *wp = new_wp();
-//	wp = (WP*)malloc(sizeof(char)*(strlen(args)+1));
-//	strcpy(args,wp->expr);
+	WP *wp = new_wp();
+	wp = (WP*)malloc(sizeof(char)*(strlen(args)+1));
+	strcpy(args,wp->expr);
 //	wp->old_val = ans;
 //	printf("Set watchpoint #%d\n",wp->NO);
 //	printf("expr   = %s\n",wp->expr);
