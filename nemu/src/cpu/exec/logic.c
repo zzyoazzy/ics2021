33dd3badc8711916375c2bd43e6c92cb0b6c0715
tracer-ics2017 <tracer@njuics.org>
2021-05-12 10:31:41 +0800

    >  compile 161930315 zhaozeyuan Linux debian 4.19.0-14-686 #1 SMP Debian 4.19.171-2 (2021-01-30) i686 GNU/Linux  10:31:41 up  1:47,  2 users,  load average: 0.06, 0.03, 0.00 88ad4fddc5ab4c8f226b413d19f0d61180c0da0

--- nemu/src/cpu/exec/logic.c
+++ nemu/src/cpu/exec/logic.c
@@ -10,9 +10,9 @@ make_EHelper(and) {
   rtl_li(&t0,0);
   rtl_set_OF(&t0);
   rtl_set_CF(&t0);
+  printf("%x",id_src->val);
   rtl_and(&id_dest->val,&id_dest->val,&id_src->val);
   rtl_update_ZFSF(&id_dest->val, id_dest->width);
-  assert(0);
   print_asm_template2(and);
 }
 
