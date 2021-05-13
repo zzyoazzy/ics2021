682e89818a3dc0be5c9acbf4316eff07120456de
tracer-ics2017 <tracer@njuics.org>
2021-05-13 21:11:00 +0800

    >  compile 161930315 zhaozeyuan Linux debian 4.19.0-14-686 #1 SMP Debian 4.19.171-2 (2021-01-30) i686 GNU/Linux  21:11:00 up  6:29,  4 users,  load average: 0.00, 0.00, 0.00 bba101b16227d6b94b3886bd430bd60c6f1e5c2b

--- nemu/src/cpu/exec/arith.c
+++ nemu/src/cpu/exec/arith.c
@@ -1,24 +1,37 @@
 #include "cpu/exec.h"
 
 make_EHelper(add) {
-  rtl_add(&id_dest->val, &id_dest->val, &id_src->val);
-  operand_write(id_dest,&id_dest->val);
+  rtl_add(&t2, &id_dest->val, &id_src->val);
+  rtl_sltu(&t0, &t2, &id_dest->val);
+  operand_write(id_dest, &t2);
+
+  rtl_update_ZFSF(&t2, id_dest->width);
+
+  rtl_set_CF(&t0);
+
+  rtl_xor(&t0, &id_dest->val, &id_src->val);
+  rtl_not(&t0);
+  rtl_msb(&t0, &t0, id_dest->width);
+  rtl_xor(&t1, &t2, &id_dest->val);
+  rtl_and(&t0, &t0, &t1); 
+  rtl_set_OF(&t0);
+
   print_asm_template2(add);
 }
 
 make_EHelper(sub) {
   rtl_sub(&t2, &id_dest->val, &id_src->val);
-  rtl_sltu(&t3, &id_dest->val, &t2);
+  rtl_sltu(&t0, &id_dest->val, &t2);
   operand_write(id_dest, &t2);
 
   rtl_update_ZFSF(&t2, id_dest->width);
 
-  rtl_sltu(&t0, &id_dest->val, &t2);
-  rtl_or(&t0, &t3, &t0);
   rtl_set_CF(&t0);
 
   rtl_xor(&t0, &id_dest->val, &id_src->val);
   rtl_msb(&t0, &t0, id_dest->width);
+  rtl_xor(&t1, &t2, &id_dest->val);
+  rtl_and(&t0, &t0, &t1); 
   rtl_set_OF(&t0);
 
   print_asm_template2(sub);
@@ -26,25 +39,36 @@ make_EHelper(sub) {
 
 make_EHelper(cmp) {
   rtl_sub(&t2, &id_dest->val, &id_src->val);
-  rtl_sltu(&t3, &id_dest->val, &t2);
+  rtl_sltu(&t0, &id_dest->val, &t2);
 
   rtl_update_ZFSF(&t2, id_dest->width);
 
-  rtl_sltu(&t0, &t2, &t3);
-  rtl_or(&t0, &t3, &t0);
   rtl_set_CF(&t0);
 
-  rtl_xor(&t0, &t2, &id_src->val);
+  rtl_xor(&t0, &id_dest->val, &id_src->val);
   rtl_msb(&t0, &t0, id_dest->width);
+  rtl_xor(&t1, &t2, &id_dest->val);
+  rtl_and(&t0, &t0, &t1); 
   rtl_set_OF(&t0);
-
   print_asm_template2(cmp);
 }
 
 make_EHelper(inc) {
   rtl_li(&t0,1);
-  rtl_add(&id_dest->val,&id_dest->val,&t0);
-  operand_write(id_dest,&id_dest->val);
+  rtl_add(&t1,&id_dest->val,&t0);
+  rtl_update_ZFSF(&t1, id_dest->width);
+  rtl_sltu(&t2, &t1, &id_dest->val);
+  operand_write(id_dest,&t1);
+  
+  rtl_set_CF(&t2);
+  
+  rtl_msb(&t2, &t1, id_dest->width);
+  rtl_msb(&t1, &id_dest->val, id_dest->width);
+  rtl_not(&t1);
+  rtl_and(&t0, &t1, &t2); 
+
+  rtl_set_OF(&t0);
+  
   print_asm_template1(inc);
 }
 
