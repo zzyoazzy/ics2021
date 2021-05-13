8d32fb5ca3fa62aac1a37165a0ef1091afd14353
tracer-ics2017 <tracer@njuics.org>
2021-05-13 18:10:03 +0800

    >  compile 161930315 zhaozeyuan Linux debian 4.19.0-14-686 #1 SMP Debian 4.19.171-2 (2021-01-30) i686 GNU/Linux  18:10:03 up  3:28,  4 users,  load average: 0.03, 0.01, 0.00 3b54d7ba65b2e47566f297d912b99a99fb0db42f

--- nemu/include/cpu/rtl.h
+++ nemu/include/cpu/rtl.h
@@ -131,7 +131,7 @@ static inline void rtl_mv(rtlreg_t* dest, const rtlreg_t *src1) {
 
 static inline void rtl_not(rtlreg_t* dest) {
   // dest <- ~dest
-  rtl_xori(dest,dest,1);
+  rtl_xori(dest,dest,~0u);
 }
 
 static inline void rtl_sext(rtlreg_t* dest, const rtlreg_t* src1, int width) {
