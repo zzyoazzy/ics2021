3e2d090395c576464634b3be1abab0ea69a2d0fa
tracer-ics2017 <tracer@njuics.org>
2021-05-13 15:27:45 +0800

    >  compile 161930315 zhaozeyuan Linux debian 4.19.0-14-686 #1 SMP Debian 4.19.171-2 (2021-01-30) i686 GNU/Linux  15:27:45 up 46 min,  3 users,  load average: 0.00, 0.00, 0.00 ef558cb82e17136a8656fd245fab94da4bb136dc

--- nemu/src/cpu/exec/arith.c
+++ nemu/src/cpu/exec/arith.c
@@ -4,6 +4,7 @@ make_EHelper(add) {
   printf("!!!!%d!!!!\n",id_dest->val);
   rtl_add(&id_dest->val, &id_dest->val, &id_src->val);
   printf("!!!!%d!!!!\n",id_dest->val); 
+  operand_write(id_dest,&id_dest->val);
   print_asm_template2(add);
 }
 
