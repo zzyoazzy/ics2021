3c9f3c54bb1b6027443c97ac5415ff4b9d5f36e1
tracer-ics2017 <tracer@njuics.org>
2021-05-16 20:05:47 +0800

    >  compile 161930315 zhaozeyuan Linux debian 4.19.0-14-686 #1 SMP Debian 4.19.171-2 (2021-01-30) i686 GNU/Linux  20:05:47 up 40 min,  3 users,  load average: 0.00, 0.04, 0.00 8e19fb72ff34260faad7eec2212a872526dc36a6

--- nemu/src/cpu/exec/exec.c
+++ nemu/src/cpu/exec/exec.c
@@ -72,10 +72,10 @@ make_group(gp7,
 /* TODO: Add more instructions!!! */
 
 opcode_entry opcode_table [512] = {
-  /* 0x00 */	EMPTY, IDEX(G2E,add), EMPTY, EMPTY,
+  /* 0x00 */	EMPTY, IDEX(G2E,add), EMPTY, IDEX(E2G,add),
   /* 0x04 */	EMPTY, IDEX(I2a,add), EMPTY, EMPTY,
-  /* 0x08 */	EMPTY, IDEX(G2E,or), IDEXW(E2G,or,1), IDEX(E2G,or),
-  /* 0x0c */	EMPTY, EMPTY, EMPTY, EX(2byte_esc),
+  /* 0x08 */	IDEXW(G2E,or,1), IDEX(G2E,or), IDEXW(E2G,or,1), IDEX(E2G,or),
+  /* 0x0c */	EMPTY, IDEX(I2a,or), EMPTY, EX(2byte_esc),
   /* 0x10 */	EMPTY, EMPTY, EMPTY, EMPTY,
   /* 0x14 */	EMPTY, EMPTY, EMPTY, EMPTY,
   /* 0x18 */	EMPTY, EMPTY, EMPTY, IDEX(E2G,sbb),
