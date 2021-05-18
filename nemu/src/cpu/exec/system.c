32e34352bdd284efd008081429f526af9f832d42
tracer-ics2017 <tracer@njuics.org>
2021-05-18 19:07:49 +0800

    >  compile 161930315 zhaozeyuan Linux debian 4.19.0-14-686 #1 SMP Debian 4.19.171-2 (2021-01-30) i686 GNU/Linux  19:07:49 up  1:21,  4 users,  load average: 0.00, 0.00, 0.00 1f92d87a461c432d61abdbfad1c8967a34f70f1

--- nemu/src/cpu/exec/system.c
+++ nemu/src/cpu/exec/system.c
@@ -54,7 +54,7 @@ make_EHelper(in) {
 }
 
 make_EHelper(out) {
-  assert(id_src->width==2);
+  assert(id_src->width==4);
   pio_write(id_dest->val,id_src->width,id_src->val);
   print_asm_template2(out);
 
