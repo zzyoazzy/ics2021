c75be2a9c31078b8af473b4f28bc03cbf36ccb14
tracer-ics2017 <tracer@njuics.org>
2021-05-19 19:23:24 +0800

    >  run 161930315 zhaozeyuan Linux debian 4.19.0-14-686 #1 SMP Debian 4.19.171-2 (2021-01-30) i686 GNU/Linux  19:23:24 up 11:22,  4 users,  load average: 0.37, 0.27, 0.11 aa3c66cfa4c1efa61fb4fad62d95255a1f18e048

--- nexus-am/apps/microbench/include/benchmark.h
+++ nexus-am/apps/microbench/include/benchmark.h
@@ -52,7 +52,6 @@ extern "C" {
 #define   MD5_LG {10000000,  16 MB, 19593, 0x27286a42}
 
 #define BENCHMARK_LIST(def) \
-  def(  md5,   "md5",   MD5_SM,   MD5_LG, "MD5 digest") \
   def(qsort, "qsort", QSORT_SM, QSORT_LG, "Quick sort") \
   def(queen, "queen", QUEEN_SM, QUEEN_LG, "Queen placement") \
   def(   bf,    "bf",    BF_SM,    BF_LG, "Brainf**k interpreter") \
